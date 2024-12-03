13697448d0d52356ae8aa20d78f470bc9d2ff349
GP-Person <garrettapearson@gmail.com>
2024-12-03 13:13:43 -0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../server/client/dist', // Adjust path so that the built files are in the server directory
+  },
   server: {
     port: 3000,
     open: true,
